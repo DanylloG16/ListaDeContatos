f6711f2f096647aa5a0dbefb8c679d9635cee136
Michael Felipe <bfr.michael@gmail.com>
2023-12-27 20:42:43 -0300

    Projeto inicial

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,55 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    private static List<Contato> listContatos = new ArrayList<>(0);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//        CRUD = CREATE (criar), READ (ler), UPDATE (atualizar), DELETE (apagar)
+
+        List<Contato> contatos = new ArrayList<>(0);
+
+        Contato contato = new Contato();
+        contato.setNome("Fulano");
+        contato.setTelefone("+55 (61) 99999-0000");
+        contato.setEmail("fulano@email.com");
+        contatos.add(contato);
+
+        Contato contato1 = new Contato();
+        contato1.setNome("Ciclano");
+        contato1.setTelefone("+55 (61) 99999-1111");
+        contato1.setEmail("ciclano@email.com");
+        contatos.add(contato1);
+
+        salvar(contatos);
+
+        Contato contatoEncontrado = buscar("C");
+
+        System.out.println("Telefone = " + contatoEncontrado.getTelefone());
+        System.out.println("E-mail = " + contatoEncontrado.getEmail());
+
+    }
+
+    private static void salvar(Contato contato) {
+        //TODO validar campos preenchidos
+        listContatos.add(contato);
+    }
+
+    private static void salvar(List<Contato> contatos) {
+        listContatos.addAll(contatos);
+    }
+
+    private static Contato buscar(String nome) {
+        for(int i = 0; i < listContatos.size(); i++) {
+            Contato contato = listContatos.get(i);
+            if(contato.getNome().equals(nome)) {
+                return contato;
+            }
+        }
+        return null;
     }
 }
